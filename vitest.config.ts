0a9d627e03d4b4debc3c5c3c188d965b2b9156f0
Krishna Tiwari <kkrishnaat001@gmail.com>
2026-09-17 21:09:36 +0530

    Remove thread pool options from Vitest config
    Removed thread pool configuration from Vitest setup.

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,14 +10,6 @@ export default defineConfig({
     setupFiles: ['./src/tests/setup.ts'],
     testTimeout: 15000,
     include: ['src/tests/**/*.test.{ts,tsx}'],
-    pool: 'threads',
-    poolOptions: {
-      threads: {
-        singleThread: true,
-        maxThreads: 1,
-        minThreads: 1,
-      },
-    },
     maxWorkers: 1,
     minWorkers: 1,
     coverage: {
